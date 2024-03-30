589ac3f35ce86ec4f8363ce451948fd55d5bcd19
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-03-30 13:33:26 +0530

    logout added

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MdLogout } from "react-icons/md";
 
 const Logout = () => {
+  const handleLogout = () => console.log("logout");
   return (
-    <div>Logout</div>
-  )
-}
+    <>
+      <img
+        src={'https://i.stack.imgur.com/frlIf.png'}
+        className="w-10 h-10 rounded-full border border-gray-800"
+      />
 
-export default Logout
+      <div
+        className="cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto border border-gray-800"
+        onClick={handleLogout}
+      >
+        <MdLogout size={22} />
+      </div>
+    </>
+  );
+};
+
+export default Logout;
