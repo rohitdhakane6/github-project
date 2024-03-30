4eadbd2ec560bcbfba4e916c73c52177eea0a469
Rohit Dhakane <rohitdhakane6@gmail.com>
2024-03-30 13:07:44 +0530

    basic setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import ExplorePage from "./pages/ExplorePage";
+import HomePage from "./pages/HomePage";
+import LikesPage from "./pages/LikesPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
-
   return (
-    <>
-    <div className="bg-red-600 h-52 w-52">hello</div>
-    </>
-  )
+    <div className="flex">
+    <Sidebar />
+    <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/likes" element={<LikesPage />} />
+      </Routes>
+    </div>
+  </div>
+  );
 }
 
-export default App
+export default App;
